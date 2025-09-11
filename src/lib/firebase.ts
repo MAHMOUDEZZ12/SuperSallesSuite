1fa5e31c162ffada2613008cf0ecbda75dc39962
mahmoud ezz <leadbylead1@gmail.com>
2025-09-11 08:54:40 +0000

    Try fixing this error: `Runtime ReferenceError: getFirestore is not defi

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore }from "firebase/firestore";
 import { firebaseConfig } from "./firebase-config";
 
 
