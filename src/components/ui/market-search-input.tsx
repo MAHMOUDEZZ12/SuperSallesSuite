d01cba61ace28c95e3282c64cc3b2978e343b818
mahmoud ezz <leadbylead1@gmail.com>
2025-09-12 01:06:11 +0000

    the box in this case need tb be wfer

--- src/components/ui/market-search-input.tsx
+++ src/components/ui/market-search-input.tsx
@@ -18,7 +18,7 @@ export default function MarketSearchInput() {
     };
     
     return (
-        <div className="max-w-3xl mx-auto relative group">
+        <div className="max-w-4xl mx-auto relative group">
             <form onSubmit={handleSearch} className="relative">
                  <div className="animated-gradient-border-wrapper">
                     <div className="relative z-10 bg-black/90 rounded-lg">
